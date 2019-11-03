072f9768b1086c53c6d6ab1fefdb98532b65b1c9
LuizR016 <50078869+LuizR016@users.noreply.github.com>
2019-11-02 22:40:22 -0300

    Add files via upload

---
+++ TGATT/routes/bancos.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require("mongoose");
+require("../models/Bancos"); 
+const Banco = mongoose.model('banco');
+const {cadBanco} = require("../helpers/cadBanco");
+const {conBanco} = require("../helpers/conBanco");
+const {delBanco} = require("../helpers/delBanco");
+const {editBanco} = require("../helpers/editBanco");
+//Cadastrar Bancos
+router.get("/cadastrarbancos", cadBanco, function(req,res){
+    res.render('../public/cadastrarbancos');
+});
+
+router.post("/validabancos", function(req,res){
+
+    const novoBanco = {
+    codBanco: req.body.CodBanco,
+    nomeBanco: req.body.NomeBanco,
+    CNPJ: req.body.CNPJ,
+    numeroBanco: req.body.NumBanco,
+    Email: req.body.Email,
+    Telefone: req.body.Telefone,
+    Tipo: req.body.tipoarq
+}
+
+new Banco(novoBanco).save().then(function(){
+res.redirect('/bancocadastrado');
+}).catch(function(erro){
+    console.log(erro);
+    req.flash("error_msg", "Erro ao cadastrar banco.");
+});
+
+});
+
+router.get("/bancocadastrado", function(req,res){
+res.render('../public/bancocadastrado')
+});
+
+//Consultar
+router.get('/consultarbancos', conBanco, function(req,res){
+    Banco.find().sort({codBanco: 'asc'}).then((banco) =>{
+       res.render('../public/consultarbancos', {banco: banco});
+       }).catch((erro) => {
+            console.log(erro);
+           req.flash("error_msg","Erro ao consultar banco.");
+       }); 
+   });
+   router.get('/consultarbancos/:busca', function(req,res){
+    if(!isNaN(req.params.busca)){    
+        Banco.find({codBanco: req.params.busca}).sort({codBanco: 'desc'}).then((banco) =>{
+            console.log(req.params.busca);
+            console.log(banco);
+        res.render('../public/consultarbancos', {banco: banco});
+        }).catch((erro) => {
+            console.log(erro);
+            req.flash("error_msg","Erro ao consultar o banco. Erro:");
+        });
+        }
+    else{
+        Banco.find({nomeBanco: new RegExp(req.params.busca, "i")}).sort({nomeBanco: 'asc'}).then((banco) =>{
+            res.render('../public/consultarbancos', {banco: banco});
+            }).catch((erro) => {
+                console.log(erro);
+                req.flash("error_msg","Erro ao consultar o banco. Erro:");
+            });
+        } 
+   });
+//Deletar
+router.get("/deletarbanco/:id", delBanco, function(req,res){
+    Banco.findOneAndDelete({_id : req.params.id}).then((banco) =>{
+        req.flash("success_msg","Banco deletado. ");
+        res.redirect('/consultarbancos');
+         }).catch((erro) =>{
+        console.log(erro);
+        req.flash("error_msg","Banco não encontrado.");
+    })
+});
+//Editar
+
+router.get("/editarbancos/:id", editBanco, function(req,res){
+    Banco.findOne({_id: req.params.id}).then((banco) =>{
+    res.render('../public/editarbancos', {banco: banco});
+    }).catch((erro) =>{
+        console.log(erro);
+        req.flash("error_msg", "Banco não encontrado.");
+        res.redirect("/consultarbancos");
+    });
+});
+
+router.post("/bancoedicao", function(req,res){
+    Banco.findOne({codBanco: req.body.CodBanco}).then((banco)=>{
+        
+        banco.codBanco = req.body.CodBanco,
+        banco.nomeBanco = req.body.NomeBanco,
+        banco.CNPJ = req.body.CNPJ,
+        banco.numeroBanco =  req.body.NumBanco,
+        banco.Email =  req.body.Email,
+        banco.Telefone =  req.body.Telefone,
+        banco.Tipo =  req.body.tipoarq
+    
+        banco.save().then(() => {
+            req.flash("success_msg","Banco editado com sucesso!");
+            res.redirect("/consultarbancos");
+        }).catch((erro) =>{
+            console.log(erro);
+            req.flash("error_msg","Banco não editado. Erro: ");
+            res.redirect("/consultarbancos");
+        })
+    })
+});
+module.exports = router
---
+++ TGATT/routes/contas.js
@@ -0,0 +1,135 @@
+const express = require('express');
+const router = express.Router();
+
+const mongoose = require("mongoose");
+require("../models/Contas"); 
+const Conta = mongoose.model('conta');
+const {cadConta} = require("../helpers/cadConta");
+const {conConta} = require("../helpers/conConta");
+const {delConta} = require("../helpers/delConta");
+const {editConta} = require("../helpers/editConta");
+//Cadastrar Contas boyz
+router.get("/cadastrarcontas", cadConta, function(req,res){
+    res.render('../public/cadastrarcontas');
+});
+
+router.post("/validacontas", function(req,res){
+
+    const novaConta = {
+    tipoConta: req.body.tipoconta,
+    codConta: req.body.CodConta,
+    valConta: req.body.ValConta,
+    nomeFornecedor: req.body.NomeFornecedor,
+    Juros: req.body.Juros,
+    Multa: req.body.Multa,
+    Desconto: req.body.Desconto,
+    Situacao: req.body.Situacao,
+    dataEmissao: req.body.DataEmissao,
+    dataVencimento: req.body.DataVencimento,
+    codigoBarras: req.body.CodBarras,
+    Observacao: req.body.Observacao,
+    Descricao: req.body.Descricao
+}
+
+new Conta(novaConta).save().then(function(){
+res.redirect('/contacadastrada');
+}).catch(function(erro){
+    console.log(erro);
+    req.flash("error_msg", "Erro ao cadastrar conta." );
+});
+
+});
+
+router.get("/contacadastrada", function(req,res){
+res.render('../public/contacadastrada')
+});
+
+//Consultar Boyzzzzzzzzzzzzzzzz
+router.get('/consultarcontas', conConta, function(req,res){
+    Conta.find().sort({codConta: 'asc'}).then((conta) =>{
+       res.render('../public/consultarcontas', {conta: conta});
+       }).catch((erro) => {
+           console.log(erro);
+           req.flash("error_msg","Erro ao consultar contas.");
+       }); 
+   });
+   router.get('/consultarcontas/:busca', function(req,res){
+    if(!isNaN(req.params.busca)){    
+        Conta.find({codConta: req.params.busca}).sort({codConta: 'desc'}).then((conta) =>{
+            console.log(req.params.busca);
+            console.log(conta);
+        res.render('../public/consultarcontas', {conta: conta});
+        }).catch((erro) => {
+            console.log(erro);
+            req.flash("error_msg","Erro ao consultar a conta. Erro:");
+        });
+        }
+    else{
+        Conta.find({NomeFornecedor: new RegExp(req.params.busca, "i")}).sort({NomeFornecedor: 'asc'}).then((conta) =>{
+            res.render('../public/consultarcontas', {conta: conta});
+            }).catch((erro) => {
+                console.log(erro);
+                req.flash("error_msg","Erro ao consultar a conta. Erro:");
+            });
+        } 
+   });
+//Deletar Boyzzzzzzzzzzzzzzzzzz
+router.get("/deletarcontas/:id", delConta, function(req,res){
+    Conta.findOneAndDelete({_id : req.params.id}).then((conta) =>{
+        req.flash("success_msg","Conta deletada. ");
+        res.redirect('/consultarcontas');
+         }).catch((erro) =>{
+        console.log(erro);
+        req.flash("error_msg","Conta não encontrada.");
+    })
+});
+//Editar BOOOOOOOOOOOOOOOOOOOOOOOOOYZZZZZZZZZZ
+router.get("/editarcontas/:id", editConta, function(req,res){
+    Conta.findOne({_id: req.params.id}).then((conta) =>{
+
+    res.render('../public/editarcontas', {conta: conta});
+    }).catch((erro) =>{
+        console.log(erro);
+        req.flash("error_msg", "Conta não encontrada.");
+        res.redirect("/consultarcontas");
+    });
+});
+
+router.post("/contaedicao", function(req,res){
+    Conta.findOne({codConta: req.body.CodConta}).then((conta)=>{
+        
+        conta.tipoConta = req.body.tipoconta
+        conta.codConta = req.body.CodConta
+        conta.valConta = req.body.ValConta
+        conta.nomeFornecedor = req.body.NomeFornecedor
+        conta.Juros = req.body.Juros
+        conta.Multa = req.body.Multa
+        conta.Desconto = req.body.Desconto
+        conta.dataEmissao = req.body.DataEmissao
+        conta.dataVencimento = req.body.DataVencimento
+        conta.codigoBarras = req.body.CodBarras
+        conta.Observacao = req.body.Observacao
+        conta.Descricao = req.body.Descricao
+    
+        conta.save().then(() => {
+            req.flash("success_msg","Conta editado com sucesso!");
+            res.redirect("/consultarcontas");
+        }).catch((erro) =>{
+            console.log(erro);
+            req.flash("error_msg","Conta não editado.");
+            res.redirect("/consultarcontas");
+        })
+    })
+});
+
+//Simulacao
+router.get("/simulacao", function(req,res){
+    Conta.find().sort({codConta: 'asc'}).then((conta) =>{
+        res.render('../public/simulacao', {conta: conta});
+        }).catch((erro) => {
+            console.log(erro);
+            req.flash("error_msg","Erro ao consultar contas.");
+        }); 
+});
+
+module.exports = router
